700979583e7481aa2647f057a9df541f84fd8b8e
Noah Bradley <noah.oli.bradley@gmail.com>
2023-06-18 16:12:34 -0400

    added users back

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 #    'crispy_bootstrap4',
 #
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
