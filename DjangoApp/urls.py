f7c62a6a617ca511299c74fe02f35b8757baec94
Noah Bradley <noah.oli.bradley@gmail.com>
2023-06-18 16:52:31 -0400

    added urlpattern

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -25,5 +25,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('', include('blog.urls')),
 ]
