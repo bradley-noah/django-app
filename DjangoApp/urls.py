4f3513e7711191aa7f56c253db1cfa34c7f10fef
Noah Bradley <noah.oli.bradley@gmail.com>
2023-06-18 16:37:27 -0400

    removed import

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from users import views as user_views
 
 #from django.contrib.auth import views as auth_views
 #from django.urls import path, include
