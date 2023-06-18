8cd2ae712091fefaade0b152507f8ee992ea2ca4
Noah Bradley <noah.oli.bradley@gmail.com>
2023-06-18 16:18:33 -0400

    added crispy template pack

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -136,3 +136,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
+
