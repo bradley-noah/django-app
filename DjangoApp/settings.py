1f36c257d1de16ada9e6207791e6d72a76adb1d3
Noah Bradley <noah.oli.bradley@gmail.com>
2023-06-19 13:47:57 -0400

    update settings

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
 #    'crispy_forms',
 #    'crispy_bootstrap4',
 #
-#    'blog.apps.BlogConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
