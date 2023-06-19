c4d0d5464aa70a825662f46bf5a8487dce1274e8
Noah Bradley <noah.oli.bradley@gmail.com>
2023-06-19 11:17:13 -0400

    security update

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cxx&o9683$v1#h72dy99hz&8@6g==bx+nk0w_a&)i9q99&&22='
+SECRET_KEY = DJANGO_SECRET_KEY
+#'django-insecure-cxx&o9683$v1#h72dy99hz&8@6g==bx+nk0w_a&)i9q99&&22='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
