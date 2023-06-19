8d3fc3e7b94d101cd7cc8d380218bfc04f642c6b
Noah Bradley <noah.oli.bradley@gmail.com>
2023-06-19 11:24:37 -0400

    fix

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -134,11 +134,11 @@ MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-#
-#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#
-#CRISPY_TEMPLATE_PACK = 'bootstrap4'
-#
-#LOGIN_REDIRECT_URL = 'blog-home'
-#LOGIN_URL = 'login'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
 
